61d962cbd55f5edd3f8e3fcf4d57d0da5b2511cb
Mercurial <sizil.krishna23@gmail.com>
2019-10-03 19:43:41 +0530

    Added themes and beautify

--- after-reinstall.sh
+++ after-reinstall.sh
@@ -13,6 +13,7 @@ BPurple='\033[1;35m'      # Purple
 BCyan='\033[1;36m'        # Cyan
 BWhite='\033[1;37m'       # White
 
+clear
 echo -e "\n${BGreen}"
 echo -e "                                           .__       .__   "
 echo -e "   _____   ___________   ____  __ _________|__|____  |  |  "
@@ -23,19 +24,22 @@ echo -e "       \/     \/            \/                     \/      "
 echo -e "${Color_Off}\n"
 
 echo -e "\n ${BGreen}1. Refreshing packages and updating system${Color_Off} \n"
-apt update && apt upgrade && apt dist-upgrade && apt autoremove
+apt update && apt -y upgrade && apt -y dist-upgrade && apt -y autoremove
 
 echo -e "\n ${BGreen}2. Installing Important Stuff${Color_Off} \n"
 
 echo -e "\n ${BCyan}2.1 Installing Drivers${Color_Off} \n"
 
-apt install firmware-linux-nonfree firmware-realtek firmware-misc-nonfree firmware-iwlwifi firmware-netxen pciutils 
+apt -y install firmware-linux-nonfree firmware-realtek firmware-misc-nonfree firmware-iwlwifi firmware-netxen pciutils 
 
 echo -e "\n ${BCyan}2.2 Installing important applications${Color_Off} \n"
 
-apt install vim compton timeshift redshift redshift-gtk apt-xapian-index gparted ntfs-3g shotwell vlc vlc-plugin-bittorrent libav-tools terminator caja-open-terminal htop usbutils tree qbittorrent locate p7zip p7zip-full unrar-free unzip git
+apt -y install vim compton timeshift redshift redshift-gtk apt-xapian-index gparted ntfs-3g shotwell vlc vlc-plugin-bittorrent libav-tools terminator caja-open-terminal htop usbutils tree qbittorrent locate p7zip p7zip-full unrar-free unzip git 
+
+#(OPTIONAL) 
+apt -y install texmaker
 
 echo -e "\n ${BGreen}3. Installing Printer Stuff${Color_Off} \n"
 apt install hplip hplip-data hplip-doc hpijs-ppds hplip-gui  printer-driver-hpcups printer-driver-hpijs printer-driver-pxljr
 
-echo -e "\n ${BGreen} All Set! Thank you.\n"
+echo -e "\n ${BGreen} All Set! Thank you.${Color_Off}\n"
